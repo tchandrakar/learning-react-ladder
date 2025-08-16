b34502d28617215de333a55bc5bd3b9a8d069559
Tishant Chandrakar <tchandrakar@Tishants-Laptop.local>
2025-08-16 18:00:35 +0530

    Filtering by Search Input

--- monster-rolodex/src/App.jsx
+++ monster-rolodex/src/App.jsx
@@ -4,18 +4,25 @@ import CardList from './components/card-list/card-list.component'
 
 const App = () => {
   const [monsters, setMonsters] = useState([]);
+  const [filteredMonsters, setFilteredMonsters] = useState([]);
 
   useEffect(() => {
     fetch('https://jsonplaceholder.typicode.com/users')
       .then((response) => response.json())
       .then((users) => {
         setMonsters(users)
+        setFilteredMonsters(users)
       })
   }, [])
 
   return (
     <>
-      <CardList monsters={monsters} />
+      <input type="search" placeholder="search monsters" onChange={(event) => {
+        const searchField = event.target.value.toLowerCase();
+        const filteredMonsters = monsters.filter((monster) => monster.name.toLowerCase().includes(searchField));
+        setFilteredMonsters(filteredMonsters);
+      }} />
+      <CardList monsters={filteredMonsters} />
     </>
   )
 }
